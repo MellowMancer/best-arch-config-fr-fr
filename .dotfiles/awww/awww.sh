49494e51b864763f35f55c0e63c170dc2acb37df
Mello <wyatharth@gmail.com>
2026-03-27 02:17:43 +0530

    updated swww to awww

--- .dotfiles/awww/awww.sh
+++ .dotfiles/awww/awww.sh
@@ -1,7 +1,7 @@
-#start swww
+#start awww
 WALLPAPERS_DIR=~/Wallpapers/current
 WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
-swww-daemon &
+awww-daemon &
 sleep 1.0 &
-swww restore || swww img "$WALLPAPER"
+awww restore || awww img "$WALLPAPER"
 
